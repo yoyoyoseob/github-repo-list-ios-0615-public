dd74eb3dc8f07df8d0376484f7288233f2163148
JimCampagno <jimcampagno@gmail.com>
2015-06-18 18:16:48 -0400

    doSomething update

---
+++ github-repo-list/FISReposTableViewController.m
@@ -0,0 +1,68 @@
+//
+//  FISReposTableViewController.m
+//  
+//
+//  Created by Joe Burgess on 5/5/14.
+//
+//
+
+#import "FISReposTableViewController.h"
+
+@interface FISReposTableViewController ()
+@end
+
+@implementation FISReposTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.tableView.accessibilityIdentifier = @"Repo Table View";
+    self.tableView.accessibilityLabel=@"Repo Table View";
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+    // Return the number of rows in the section.
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"basicCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
